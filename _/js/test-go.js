ebe9861fac93103bc3facf096a33ef6f812b9637
ⓕⓣ <folktrash@gmail.com>
2013-08-29 08:08:41 -0700

    Testing alternate method locally by having iframes in DOM onload instead of added via js after. Just hidden. JS unhides teh appropropriate frame.

--- _/js/test-go.js
+++ _/js/test-go.js
@@ -14,13 +14,16 @@ var go = function () {
 	document.getElementById('h1').className += ' hide';
 	document.getElementById('h2').className += ' hide';
 
+	var ifd = document.getElementById('ifd');
+	var ifm = document.getElementById('ifm');
+
 	if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 		document.getElementById('h3').className += ' hide';//also hide this for desktop
 		document.getElementById('clp').className += ' hide';
 		document.getElementById('hdm').className += ' hide';
-		document.getElementById('h1').insertAdjacentHTML('afterend', '<iframe width="100%" height="100%" src="http:&#x2F;&#x2F;www.kickstarter.com&#x2F;projects&#x2F;hdilla&#x2F;wrong-side-up-a-dust-bowl-inspired-short-film&#x2F;widget&#x2F;video.html" frameborder="0"> </iframe>');
+		ifd.className = ifd.className.replace( /(?:^|\s)hide(?!\S)/ , '');
 	} else {
-		document.getElementById('h1').insertAdjacentHTML('afterend', '<iframe width="285" height="214" src="http:&#x2F;&#x2F;www.kickstarter.com&#x2F;projects&#x2F;hdilla&#x2F;wrong-side-up-a-dust-bowl-inspired-short-film&#x2F;widget&#x2F;video.html" frameborder="0"> </iframe>');
+		ifm.className = ifm.className.replace( /(?:^|\s)hide(?!\S)/ , '');
 	}
 };
 go();
